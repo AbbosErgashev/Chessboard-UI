d9fff5895067117385ca456413ffcd36bff54e56
eaxusniddinovich <eaxusniddinovich@gmail.com>
2024-01-30 12:48:45 +0500

    Add project files.

---
+++ Chessboard/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Chessboard;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void image_MouseEnter(object sender, MouseEventArgs e)
+    {
+
+    }
+}
